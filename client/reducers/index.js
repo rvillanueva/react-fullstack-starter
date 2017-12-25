1e9a562684ec7b618764f95587777e467dc7542f
Ryan Villanueva <ryanvill@gmail.com>
2017-12-25 14:52:44 -0800

    Removed error reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 import fuelSavings from './fuelSavingsReducer';
 import auth from './authReducer';
 import users from './userReducer';
-import errors from './errorReducer';
 import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
   fuelSavings,
   auth,
-  errors,
   users,
   routing: routerReducer
 });
